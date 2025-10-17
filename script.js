088229b9580507be67dececaf9c8bb5e7c352852
Mohamad Reffa <147628487+rffaprta@users.noreply.github.com>
2025-10-17 11:09:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// const buttonCLick = document.querySelector("button");
+// buttonCLick.addEventListener("click", (e) => {
+//   alert("Form submitted successfully!");
+// });
+
+const submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", (e) => {
+  alert("Form submitted successfully!");
+});
